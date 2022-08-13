b0ed93dcd451457b018d102d7d01edd44ec4f1cf
Franklin-Amador <joelamdor55@gmail.com>
2022-08-12 21:13:29 -0600

    avance

--- router/motoristas-router.js
+++ router/motoristas-router.js
@@ -41,7 +41,31 @@ router.put('/:id', (req,res) => {
             res.send("hola")
             res.end()
         }) 
-})
+});
+
+
+//servicio para crear un nuevo motorista 
+router.post('/', function(req,res,){
+    let m = new motorista({
+        nombre: req.body.nombre,
+         correo: req.body.correo,
+         contraseña: req.body,contraeña,
+         estado: req.body.estado,
+         latitud: req.body.latitud,
+         longitud: req.body.longitud, 
+         ordenes: req.body.ordenes,
+         pedidos: req.body.pedidos,
+         matricula: req.body.matricula   
+    });
+        m.save()
+        .then( (data)=> {
+            res.send(data);
+            res.end();
+        }).catch(err=> {
+            res.send(err);
+            res.end();
+        });
+});
 
 
 module.exports = router;
